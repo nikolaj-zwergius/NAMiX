12875470466d69e844b9bad0fd469538643c5942
Nikolaj Holck Zwergius <nikolajzwergius@gmail.com>
2024-09-22 15:30:46 +0200

    change to not have default

--- namix_driver.py
+++ namix_driver.py
@@ -5,6 +5,7 @@ def phenix(map,res):
     subprocess.run("phenix.real_space_refine")
 
 namex_config = "path_to_config"
-def qrnas(config = namex_config):
+def qrnas(config):
     subprocess.run("QRNA")
     print("done")
+
